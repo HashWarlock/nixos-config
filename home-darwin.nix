10a8e8072b67c3968c3a96620af33cdb4fa42b82
HashWarlock <hashwarlock@protonmail.com>
2022-12-06 21:31:09 -0600

    minor fix

--- home-darwin.nix
+++ home-darwin.nix
@@ -5,27 +5,7 @@
   home.packages = with pkgs; [
     cachix
     lazygit
-    fzf
-    git
-    git-lfs
-    exa
-    bat
   ];
-  
-  programs.git = import programs/git.nix;
-  programs.gpg = import programs/gpg.nix;
-  programs.zsh = import programs/zsh.nix;
-  programs.kitty = import programs/kitty.nix { isDarwin = true; };
+
   programs.helix = import programs/helix.nix inputs.helix.packages.${pkgs.system}.default;
-  
-  # Hide "last login" message on new terminal.
-  home.file.".hushlogin".text = "";
-     
-  # programs.ssh doesn't work well for darwin.
-  home.file.".ssh/config".text = ''
-    Host *
-      AddKeysToAgent yes
-      UseKeychain yes
-      IdentityFile ~/.ssh/id_ed25519  
-  '';
 }
